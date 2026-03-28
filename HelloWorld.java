a8a0d11d7e9933e12a9f64ddff3d8e2d3e1a8fed
gcp-deb <gcppractice13@gmail.com>
2026-03-28 11:12:49 +0530

    Add user greeting feature at the beginning

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String userName = scanner.nextLine();
+        System.out.println("Welcome, " + userName + "!");
+        
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java programming using copilot.");
         
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a value for variableA: ");
         int variableA = scanner.nextInt();
         int variableB = 20;
